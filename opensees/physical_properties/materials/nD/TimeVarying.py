e621acb7b99f5b31399abd7646b0bb4f44f5edf2
jihirmasl <jihirmas@miuandes.cl>
2024-12-06 18:12:30 -0300

    Add material tag attribute to TimeVarying and validate list lengths

--- opensees/physical_properties/materials/nD/TimeVarying.py
+++ opensees/physical_properties/materials/nD/TimeVarying.py
@@ -2,8 +2,22 @@ import PyMpc.Units as u
 from PyMpc import *
 from mpc_utils_html import *
 import opensees.utils.tcl_input as tclin
+import PyMpc
 
 def makeXObjectMetaData():
+    
+    at_material = MpcAttributeMetaData()
+    at_material.type = MpcAttributeType.Index
+    at_material.name = 'matTag1'
+    at_material.group = 'Optional parameters'
+    at_material.description = (
+        html_par(html_begin()) +
+        html_par(html_boldtext('material tag')+'<br/>') +
+        html_par('Select the material to apply the time varying') +
+        html_end()
+        )
+    at_material.indexSource.type = MpcAttributeIndexSourceType.PhysicalProperty
+    at_material.indexSource.addAllowedNamespace("materials.nD")
 	
     # E
     at_list_of_E_values = MpcAttributeMetaData()
@@ -14,7 +28,6 @@ def makeXObjectMetaData():
         html_par(html_begin()) +
         html_par(html_boldtext('list_of_E_values')+'<br/>') + 
         html_par('E values') +
-        html_par(html_href('http://opensees.berkeley.edu/wiki/index.php/Elastic_Isotropic_Material','Elastic Isotropic Material')+'<br/>') +
         html_end()
         )
 
@@ -27,7 +40,6 @@ def makeXObjectMetaData():
         html_par(html_begin()) +
         html_par(html_boldtext('list_of_v_values')+'<br/>') + 
         html_par('v values') +
-        html_par(html_href('http://opensees.berkeley.edu/wiki/index.php/Elastic_Isotropic_Material','Elastic Isotropic Material')+'<br/>') +
         html_end()
         )
     
@@ -40,7 +52,6 @@ def makeXObjectMetaData():
         html_par(html_begin()) +
         html_par(html_boldtext('list_of_A_values')+'<br/>') + 
         html_par('A values') +
-        html_par(html_href('http://opensees.berkeley.edu/wiki/index.php/Elastic_Isotropic_Material','Elastic Isotropic Material')+'<br/>') +
         html_end()
         )
     
@@ -54,7 +65,6 @@ def makeXObjectMetaData():
         html_par(html_begin()) +
         html_par(html_boldtext('ti')+'<br/>') + 
         html_par('Time intervals') +
-        html_par(html_href('http://opensees.berkeley.edu/wiki/index.php/Elastic_Isotropic_Material','Elastic Isotropic Material')+'<br/>') +
         html_end()
         )
 
@@ -67,7 +77,6 @@ def makeXObjectMetaData():
         html_par(html_begin()) +
         html_par(html_boldtext('it')+'<br/>') + 
         html_par('Initial Time') +
-        html_par(html_href('http://opensees.berkeley.edu/wiki/index.php/Elastic_Isotropic_Material','Elastic Isotropic Material')+'<br/>') +
         html_end()
         )
 
@@ -80,7 +89,6 @@ def makeXObjectMetaData():
         html_par(html_begin()) +
         html_par(html_boldtext('ft')+'<br/>') + 
         html_par('Final Time') +
-        html_par(html_href('http://opensees.berkeley.edu/wiki/index.php/Elastic_Isotropic_Material','Elastic Isotropic Material')+'<br/>') +
         html_end()
         )
 
@@ -94,15 +102,22 @@ def makeXObjectMetaData():
     xom.addAttribute(at_ti)
     xom.addAttribute(at_it)
     xom.addAttribute(at_ft)
+    xom.addAttribute(at_material)
 
 	
     return xom
+
+def checkLength(listOfValue, intervals, listName):
+    if len(listOfValue) != intervals:
+        raise Exception(f'Error: length of list of values ({listName}) should be equal to the number of intervals')
 
 def writeTcl(pinfo):
 	
     #nDMaterial ElasticIsotropic $matTag $E $v 
     xobj = pinfo.phys_prop.XObject
     tag = xobj.parent.componentId
+    
+    material = xobj.getAttribute('matTag1')
 
     # mandatory parameters
     list_of_E_values_at = xobj.getAttribute('list_of_E_values')
@@ -135,9 +150,13 @@ def writeTcl(pinfo):
         raise Exception('Error: cannot find "ft" attribute')
     ft = ft_at.real
     
+    checkLength(listValueE, ti, 'list_of_E_values')
+    checkLength(listValuev, ti, 'list_of_v_values')
+    checkLength(listValueA, ti, 'list_of_A_values')
+    
     # optional paramters
     sopt = ''
-    material_selected = '1'
+    material_selected = material.index
     str_tcl = f'{pinfo.indent}nDMaterial TimeVarying {tag} {material_selected} {int(ti)} {it} {ft} ' + ' '.join([f'{listValueE.valueAt(i)}' for i in range(int(ti))]) + ' ' + ' '.join([f'{listValuev.valueAt(i)}' for i in range(int(ti))]) + ' ' + ' '.join([f'{listValueA.valueAt(i)}' for i in range(int(ti))]) + f' {sopt}\n'
     
     # now write the string into the file
